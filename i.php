83ffbdf38ac4117f4ee4e23b5267367e6bd9566d
Mark Zhao <zhaody901@126.com>
2013-07-24 22:49:10 +0800

    干掉测试文件

--- i.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_set_cookie_params(4000);
-session_start();
-$_SESSION['test'] = '12345';
-var_dump($_SESSION);
--- o.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-var_dump($_SESSION);
-var_dump(session_get_cookie_params());
